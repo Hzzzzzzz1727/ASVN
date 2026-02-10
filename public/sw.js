55edc1bd2fc2c1b995f7d2eb996dfe595befa44c
Hzzzzzzz1727 <km441706@gmail.com>
2026-02-10 12:55:33 +0700

    Thêm PWA: manifest, sw, icons

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+// sw.js - Service Worker đơn giản
+const CACHE_NAME = 'bhtv-danang-v1';
+const urlsToCache = ['/', '/index.html'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
